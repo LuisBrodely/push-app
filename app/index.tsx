d735ae24093d5d6a8a89466514eb5681b09b88a0
LuisBrodely <luisbrodely2@gmail.com>
2024-12-19 13:42:48 -0600

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { ThemedText } from '@/components/ThemedText'
+import { View, Text } from 'react-native'
+
+const PushApp = () => {
+  return (
+    <View>
+      <ThemedText>PushApp</ThemedText>
+    </View>
+  )
+}
+
+export default PushApp
